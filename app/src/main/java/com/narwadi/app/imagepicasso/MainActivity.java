8f0af1d7324cc62fb9d031cbf459e02fe9b21951
narwadi <teguhnarwadi@gmail.com>
2016-03-08 15:36:20 +0700

    Commit ketiga

--- app/src/main/java/com/narwadi/app/imagepicasso/MainActivity.java
+++ app/src/main/java/com/narwadi/app/imagepicasso/MainActivity.java
@@ -2,13 +2,23 @@ package com.narwadi.app.imagepicasso;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageViewPicasso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageViewPicasso = (ImageView) findViewById(R.id.ImageView_picasso);
+
+        Picasso.with(this)
+                .load("https://cms-assets.tutsplus.com/uploads/users/21/posts/19431/featured_image/CodeFeature.jpg")
+                .into(imageViewPicasso);
     }
 }
